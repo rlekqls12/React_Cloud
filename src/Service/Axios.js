ae5c9b615d5a4ec881225f06d8b64cedd8917ae9
Ki, Dabin <35262508+rlekqls12@users.noreply.github.com>
2020-10-26 14:28:58 +0900

    Update Axios.js
    error fix: request affects axios

--- src/Service/Axios.js
+++ src/Service/Axios.js
@@ -2,18 +2,15 @@ import axios from 'axios';
 import https from 'https';
 import { SERVER_URL } from 'src/Functions/common.js';
 
-const request = (function() {
-  axios.defaults.withCredentials = true;
-  axios.defaults.httpsAgent = new https.Agent({
+const request = axios.create({
+  withCredentials: true,
+  httpsAgent: new https.Agent({
     rejectUnauthorized: false
-  });
-  axios.defaults.baseURL = SERVER_URL;
-  axios.defaults.headers = {
+  }),
+  baseURL: SERVER_URL,
+  headers: {
     "Content-Type": "application/x-www-form-urlencoded;multipart/form-data;text/plain;application/json;"
   }
-  axios.create();
-
-  return axios;
-})();
+});
 
 export { axios, request };
